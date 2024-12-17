86fa8f01d64b173c4d82c3edabd19deabe42fd41
toniismailz <tonismail20@gmail.com>
2024-12-17 09:56:10 +0700

    update api

--- src/api/api-satu.js
+++ src/api/api-satu.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
 
-    baseURL: "https://gwkong.pusri.co.id/service-iam-dev",
+    baseURL: process.env.NEXT_PUBLIC_API_SATU,
     headers: {
         'Authorization': process.env.NEXT_PUBLIC_KEY_SVC_IAM,
     },
